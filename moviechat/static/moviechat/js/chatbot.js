a34e0c339c0f41b3feba8802f9f1906012d304e1
Katie House <katiehouse3@gmail.com>
2019-12-04 12:45:22 -0500

    add new baseline

--- moviechat/static/moviechat/js/chatbot.js
+++ moviechat/static/moviechat/js/chatbot.js
@@ -1,16 +1,52 @@
-//#4 Initialization
-var botui = new BotUI('hello-world');
-//#5 Start chat
-botui.message.add({
-    content: 'Hello World from bot! What is your name?'
-}).then(function () {
-    return botui.action.text({
-        action: {
-            placeholder: 'Your name'
-        }
-    });
-}).then(function (res) {
+$(document).ready(function () {
+    // Initialization
+    var botui = new BotUI('hello-world');
+
+    // Start chat
     botui.message.add({
-        content: 'Hello, ' + res.value + '!'
-    });
+        content: 'Hi I am a chatbot!'
+    })
+
+    // Declare conversation function
+    function conversation() {
+        var stop = false;
+        botui.action.text({
+            action: {
+                placeholder: ''
+            }
+        }).then(function (res) {
+            if (res.value == 'goodbye') {
+                stop = true;
+                return false;
+            }
+            else {
+                // send ajax request to nlp model
+                $.ajax({
+                    url: '/ajax/api_chat_response/',
+                    data: {
+                        'userinput': res.value
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                        botui.message.add({
+                            content: data
+                        });
+                    },
+                })
+            }
+        }).then(function () {
+            // user would like to keep talking
+            if (stop == false) {
+                conversation()
+            }
+            // user would like to stop talking
+            else {
+                botui.message.add({
+                    content: 'Goodbye, my friend!'
+                });
+            }
+        })
+    }
+
+    conversation();
 });
