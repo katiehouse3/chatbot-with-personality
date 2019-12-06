4765870f1779522c1c1d7c82d5070fd61aca84ec
Katie House <katiehouse3@gmail.com>
2019-12-05 20:48:06 -0500

    add user testing

--- moviechat/models.py
+++ moviechat/models.py
@@ -1,3 +1,54 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class UserEval(models.Model):
+    #author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    MODELS = (
+        ('A', 'ModelA'),
+        ('B', 'ModelB'),
+        ('C', 'ModelC'),
+    )
+
+    GENRES = (
+        ('comedy', 'comedy'),
+        ('sport', 'Sport'),
+        ('biography', 'Biography'),
+        ('romance', 'Romance'),
+        ('action', 'Action'),
+        ('adventure', 'Adventure'),
+        ('drama', 'Drama'),
+        ('sci-fi', 'Sci-fi'),
+        ('family', 'Family'),
+        ('fantasy', 'Fantasy'),
+        ('musical', 'Musical'),
+        ('crime', 'Crime'),
+        ('thriller', 'Thriller'),
+        ('short', 'Short'),
+        ('western', 'Western'),
+        ('documentary', 'Documentary'),
+        ('horror', 'Horror'),
+        ('animation', 'Animation'),
+        ('film-noir', 'Film-noir'),
+        ('music', 'Music'),
+        ('war', 'War'),
+        ('mystery', 'Mystery')
+    )
+
+    # User first and last name
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    # User scores for the testing
+    semantic_score = models.IntegerField()
+    syntactic_score = models.IntegerField()
+    fun_score = models.IntegerField()
+    genre_score = models.IntegerField()
+
+    # Which model and genrue the user is evaluating
+    model = models.CharField(max_length=1, choices=MODELS)
+    genre =  models.CharField(max_length=100, choices=GENRES)
+
+    # Keep track of when the user evaluates the model
+    created_date = models.DateTimeField(default=timezone.now)
